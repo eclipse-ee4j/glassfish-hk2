68bec0c1f042aa3e84350ffdf81ca4da8701ca91
Patrik Dudits <patrik.dudits@payara.fish>
2019-09-12 14:45:28 +0200

    Retry bundle start on Felix' global lock race condition
    When starting multiple OSGi modules in parallel (like RunLevel service
    tends to do), Apache Felix may run into lock race condition that is
    demonstrated by throwing
    
    org.osgi.framework.BundleException: Unable to acquire global lock for resolve.
    
    The operation should be retried in such case.
    
    Signed-off-by: Patrik Dudits <patrik.dudits@payara.fish>

--- osgi/adapter/src/main/java/org/jvnet/hk2/osgiadapter/OSGiModuleImpl.java
+++ osgi/adapter/src/main/java/org/jvnet/hk2/osgiadapter/OSGiModuleImpl.java
@@ -174,7 +174,7 @@ public class OSGiModuleImpl implements HK2Module {
                     AccessController.doPrivileged(new PrivilegedExceptionAction(){
                         public Object run() throws BundleException
                         {
-                            bundle.start(Bundle.START_TRANSIENT);
+                            startBundle();
                             return null;
                         }
                     });
@@ -182,7 +182,7 @@ public class OSGiModuleImpl implements HK2Module {
                     throw (BundleException)e.getException();
                 }
             } else {
-                bundle.start(Bundle.START_TRANSIENT);
+                startBundle();
             }
             isTransientlyActive = true;
             if (logger.isLoggable(Level.FINE)) {
@@ -212,6 +212,39 @@ public class OSGiModuleImpl implements HK2Module {
             lifecyclePolicy.start(this);
         }
         return;
+    }
+
+    private void startBundle() throws BundleException {
+        BundleException exception = null;
+        for(int attempt = 1; attempt <= 3; attempt++) {
+            try {
+                if (attempt > 1) {
+                    if (logger.isLoggable(Level.FINER)) {
+                        logger.log(Level.FINER, "Retrying start of " + bundle + " due to lock race condition");
+                    }
+                    jitter();
+                }
+                bundle.start(Bundle.START_TRANSIENT);
+                return;
+            } catch (BundleException be) {
+                exception = be;
+                // When starting bundles in multiple threads, Apache Felix may run in locking race
+                // condition throwing "Unable to acquire global lock" on one of competing threads.
+                // Best course of action is to retry bundle start rather than fail unrecoverably.
+                if (!be.getMessage().contains("Unable to acquire global lock")) {
+                    break;
+                }
+            }
+        }
+        throw exception;
+    }
+
+    private void jitter() {
+        try {
+            Thread.sleep(new Random().nextInt(20));
+        } catch (InterruptedException ie) {
+            // nevermind
+        }
     }
 
     private String toString(int state)
