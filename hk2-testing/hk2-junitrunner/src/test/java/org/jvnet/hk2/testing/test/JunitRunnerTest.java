adf985ae281d3addc8ff855c5d98c280ec5de7b3
Attila Houtkooper <attila@roughdot.nl>
2019-10-19 09:51:00 +0200

    Added simple test for JUnitRunner
    Signed-off-by: Attila Houtkooper <attila@roughdot.nl>

---
+++ hk2-testing/hk2-junitrunner/src/test/java/org/jvnet/hk2/testing/test/JunitRunnerTest.java
@@ -0,0 +1,21 @@
+package org.jvnet.hk2.testing.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.jvnet.hk2.testing.junit.HK2JunitRunner;
+
+import javax.inject.Inject;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(HK2JunitRunner.class)
+public class JunitRunnerTest {
+
+    @Inject
+    private SimpleService injectMe;
+
+    @Test
+    public void testServiceInjection() {
+        assertNotNull(injectMe);
+    }
+}
