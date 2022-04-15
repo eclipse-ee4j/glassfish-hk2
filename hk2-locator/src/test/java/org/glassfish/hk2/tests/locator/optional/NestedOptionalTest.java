2976e7b334dc5c1ceefe279f1b7cada8478f0dbd
Piotrek Zygielo <piotr.zygielo@gmail.com>
2022-04-15 20:06:25 +0200

    Un-mix TestNG Assert from JUnit Tests

--- hk2-locator/src/test/java/org/glassfish/hk2/tests/locator/optional/NestedOptionalTest.java
+++ hk2-locator/src/test/java/org/glassfish/hk2/tests/locator/optional/NestedOptionalTest.java
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2020 Contributors to the Eclipse Foundation.
+ * Copyright (c) 2020, 2022 Contributors to the Eclipse Foundation.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -20,7 +20,7 @@ import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.testng.Assert;
+import org.junit.Assert;
 
 /** @author Balthasar Schüss */
 public class NestedOptionalTest {
